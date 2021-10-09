33110de35383151b100ba2d930b6573b275c9643
naokimizumoto <shuibenshangxi@gmail.com>
2021-10-09 18:09:42 +0000

    {start}3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,35 @@
 class BooksController < ApplicationController
-  
+ def new
+ end
+
+ def index
+   
+ end
+
+ def create
+    # １. データを新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    @book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to book_path(@book.id)
+ end
+
+ def show
+ end
+
+ def destroy
+ end
+
+ def update
+ end
+
+ def edit
+ end
+
+private
+def book_params
+  params.permit(:title, :body)
 end
-git
+
+end
