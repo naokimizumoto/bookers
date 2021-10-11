90240ca3328b9e43223e96e8a74fc146eb0296b2
naokimizumoto <shuibenshangxi@gmail.com>
2021-10-11 05:45:17 +0000

    {finish}3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,36 @@
 class BooksController < ApplicationController
- def new
- end
-
  def index
   @books = Book.all
-
+  @book = Book.new
  end
 
- def create
-    # １. データを新規登録するためのインスタンス作成
-    @book = Book.new(book_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    @book.save
-    # ３. トップ画面へリダイレクト
-    redirect_to book_path(@book.id)
- end
+  def create
+   @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'book was created successfully'
+    else
+    @books = Book.all
+    render :index
+    end
+  end
 
  def show
    @book = Book.find(params[:id])
  end
 
  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
  end
 
  def update
-    book = Book.find(params[:id])
-    book.update(list_params)
-    redirect_to book_path(list.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'book was created successfully'
+    else
+    render :edit
+    end
  end
 
  def edit
@@ -34,8 +38,7 @@ class BooksController < ApplicationController
  end
 
 private
-def book_params
-  params.permit(:title, :body)
-end
-
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
 end
